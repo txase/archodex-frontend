f5d235e2335135aaa41b55eec7654da32df50061
Chase Douglas <chasedouglas@gmail.com>
2025-11-06 15:34:00 -0800

    Skip auth service worker in playground
    This reduces the chance someone can't try the playground due to an issue
    with service workers. It's a minor concern, but I saw one error report,
    so I figured it was worth doing.

--- src/main.tsx
+++ src/main.tsx
@@ -19,32 +19,34 @@ if (import.meta.env.VITE_POSTHOG_PROJECT_API_KEY) {
   });
 }
 
-if ('serviceWorker' in navigator) {
-  const authServiceWorkerScript = import.meta.env.DEV ? '/auth-service-worker.ts' : '/auth-service-worker.js';
-  const authServiceWorkerURL = new URL(authServiceWorkerScript, location.origin);
-  try {
-    void navigator.serviceWorker.register(authServiceWorkerURL.href, { type: 'module' });
-  } catch (err) {
-    console.error('Error registering auth service worker: ', err);
-    posthog.captureException(err);
+if (!isPlayground) {
+  if ('serviceWorker' in navigator) {
+    const authServiceWorkerScript = import.meta.env.DEV ? '/auth-service-worker.ts' : '/auth-service-worker.js';
+    const authServiceWorkerURL = new URL(authServiceWorkerScript, location.origin);
+    try {
+      void navigator.serviceWorker.register(authServiceWorkerURL.href, { type: 'module' });
+    } catch (err) {
+      console.error('Error registering auth service worker: ', err);
+      posthog.captureException(err);
+      document.body.innerHTML =
+        '<h1 style="color: red;">Error registering auth service worker. See console for details.</h1>';
+    }
+  } else {
+    posthog.captureException(new Error('Service workers are not supported by this browser'));
     document.body.innerHTML =
-      '<h1 style="color: red;">Error registering auth service worker. See console for details.</h1>';
+      '<h1 style="color: red;">Service workers are not supported by this browser. Please use a modern browser such as Chrome, Firefox, Edge, or Safari.</h1>';
   }
-} else {
-  posthog.captureException(new Error('Service workers are not supported by this browser'));
-  document.body.innerHTML =
-    '<h1 style="color: red;">Service workers are not supported by this browser. Please use a modern browser such as Chrome, Firefox, Edge, or Safari.</h1>';
-}
 
-navigator.serviceWorker.ready
-  .then(() => {
-    console.debug('Auth service worker is ready');
-  })
-  .catch((err: unknown) => {
-    console.error('Error waiting for auth service worker to become ready: ', err);
-    document.body.innerHTML =
-      '<h1 style="color: red;">Error initializing auth service worker. See console for details.</h1>';
-  });
+  navigator.serviceWorker.ready
+    .then(() => {
+      console.debug('Auth service worker is ready');
+    })
+    .catch((err: unknown) => {
+      console.error('Error waiting for auth service worker to become ready: ', err);
+      document.body.innerHTML =
+        '<h1 style="color: red;">Error initializing auth service worker. See console for details.</h1>';
+    });
+}
 
 const root = document.getElementById('root');
 if (!root) {
